9042c9a30c173dd06b74c63b6760de369760652e
ThallesMach <35805076+Thalles130@users.noreply.github.com>
2018-02-13 00:08:40 -0200

    Initial commit

---
+++ JavPart3_alura_Heranca_Interface/bytebank-herdado/src/TestaFuncionario.java
@@ -0,0 +1,19 @@
+
+public class TestaFuncionario {
+
+	public static void main(String[] args) {
+
+
+		Funcionario nico = new Funcionario();
+		nico.setNome(" Nico Steppat ");
+		nico.setCpf(" 123456789-11");
+		nico.setSalario(2600.00);
+		
+		System.out.println(nico.getNome() );
+		System.out.println(nico.getBonificacao() );
+		
+		
+		
+	}
+
+}
